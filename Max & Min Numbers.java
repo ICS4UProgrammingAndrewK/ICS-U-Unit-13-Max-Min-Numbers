dcb2cc7ed33ff8af85c1e428288aa30b34214749
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-10-28 12:45:44 -0400

    Update Max & Min Numbers.java

--- Max & Min Numbers.java
+++ Max & Min Numbers.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 /**
  * Class Max & Min Numbers
@@ -8,37 +9,55 @@ import java.util.Scanner;
 * @version 1.0
 * @since   2020-10-17
 */
-public class GetMaxVal {
-	public static void main(String[] args) 
-	{
-		//This read the input from the console
-		Scanner userInput = new Scanner(System.in);
-		
-		// declare int
-		int maxValue = number[99];
-		int Counter;
-		
-		//
-		for(int Counter = 1; Counter < numbers.length; Counter++)
-		{
-			maxValue = number[Counter];
-		}
-		return maxValue;
-	}
+public class MaxMinNumbers {
+  public static void main(String[] args) {
+    
+    //This genarate number between 1 and 10
+    int num = 100;
+    Random rand = new Random();
+    
+    //Array
+    int[] numbers = new int[10];
+    
+    //for loop that generate the numbers into and array
+    for(int counter = 0; counter < numbers.length; counter++) {
+      //generate the random number
+      int random = rand.nextInt(num);
+      numbers[counter] = random;
+      System.out.println(random);
+    }
+    
+    //this is calling the Max val function
+    int maxValue = GetMaxValue(numbers);
+    int minValue = GetMinVal(numbers);
+    
+    //Display the value
+    System.out.println(" The maxValue is: " + maxValue);
+    System.out.println(" The minValue is: " + minValue);
+  }
+  public static int GetMinVal(int[] numbers) {
+    //declare min
+    int minValue = 100;
+    
+    //for loop
+    for (int counter = 1; counter < numbers.length; counter++) {
+      if (numbers[counter] < minValue){
+        minValue = numbers[counter];
+      }
+    }
+    return minValue;
+  }
+  
+public static int GetMaxValue(int[] numbers) {
+    // declare max
+    int maxValue = 0;
+    
+    //if array value is greater max val set it to array value
+    for(int counter = 1; counter < numbers.length; counter++) {
+      if (numbers[counter] > maxValue){
+        maxValue = numbers[counter];
+      }
+    }
+    return maxValue;
+  }
 }
-	public static int GetMinVal(int[] numbers)
-	{
-		//
-		int minValue = numbers[1];
-		
-		//
-		for (int Counter = 1; Counter < numbers.length; Counter++))
-		{
-			if (number[Counter] < minValue)
-			{
-				minValue = number[Counter];
-				
-			}
-			return minValue;
-		}
-	}
